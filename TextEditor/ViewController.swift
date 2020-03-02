726fba9523625706d0128ad72a29c67023e13913
Artem Nikiforov <firexpl@coccoc.com>
2020-03-02 22:50:28 +0700

    Initial Commit

---
+++ TextEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextEditor
+//
+//  Created by Артём Никифоров on 3/2/20.
+//  Copyright © 2020 firexpl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
